78173326ce233a8120a7ca9f147bbd4b9566d0b1
alakdam07 <smashalak2003@yahoo.com>
2021-04-26 10:15:13 +0300

    refector code

--- src/common/cache.ts
+++ src/common/cache.ts
@@ -1,17 +1,17 @@
 import { Image } from "react-native";
 import { Asset } from "expo-asset";
 import * as Font from "expo-font";
+import { IFonts } from "./types";
 
 export const cacheImages = (images: []) => {
-  return images.map((image: any) => {
+  return images.map((image: string) => {
     if (typeof image === `string`) {
       return Image.prefetch(image);
     }
-
     return Asset.fromModule(image).downloadAsync();
   });
 };
 
-export const cacheFonts = (fonts: any) => {
-  return fonts.map((font: any) => Font.loadAsync(font));
+export const cacheFonts = (fonts: IFonts[]) => {
+  return fonts.map((font: IFonts["ionicons"]) => Font.loadAsync(font));
 };
